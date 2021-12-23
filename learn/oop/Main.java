388a13fbbaabb31c0863944ebd5be5515476e077
morphhyy <imbibek05@gmail.com>
2021-12-23 23:41:43 +0545

    oop

--- learn/oop/Main.java
+++ learn/oop/Main.java
@@ -1,3 +1,16 @@
-public class Main {
-    
-}
+class Main {
+    public int add(int a, int b) {
+      int sum = a + b;
+      return sum;
+    }
+  
+    public static void main(String[] args) {
+      
+      int num1 = 20;
+      int num2 = 10;
+  
+      Main obj = new Main();
+      int result = obj.add(num1, num2);
+      System.out.println("Sum: " + result);
+    }
+  }
